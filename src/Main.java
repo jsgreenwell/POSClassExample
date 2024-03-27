285122cdc5740aeb4b9ec3653924e30bf3683a35
jgreenwell <jsgreenwell83@gmail.com>
2024-03-27 14:01:29 -0500

    fixing git error

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
+    /**
+     * This is a simple POS emulator for Teaching:
+     * <li>GitHub</li>
+     * <li>Classes</li>
+     * <li>Data Structures</li>
+     * It starts with providing services and their costs then students add products and sales.
+     *
+     * @param args Passed arguments - might have some at end
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Services services = new Services();
+        Scanner scan = new Scanner(System.in);
+
+        long price = 0;
+        // TODO: 1st: Change order to a list for all selected services here
+        String order = "";
+
+        // Showing a do-while application
+        do {
+            System.out.println("Welcome to our store, we offer the following services: ");
+
+            // Loop (foreach) over constant services and print each option
+            for (var serv : services.SERVICES) {
+                System.out.printf("\tOption is: %s\n", serv);
+            }
+
+            System.out.print("Which service do you want?");
+            // TODO: 3rd: after order working & our arrays are changed to a List - validate choice
+            order += scan.nextLine() + " ";
+
+            // TODO: 4 = also add a check (if) that sees if order is same size as SERVICES
+            // TODO: finally - make this work for answers like: "Yes I do" or "I do, Yes"
+            System.out.print("Have you finished your order? (yes/no)");
+        } while (!scan.nextLine().equalsIgnoreCase("yes"));
+
+        System.out.println(order);
     }
 }
--- src/Services.java
+++ src/Services.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Services {
+// Will be filled out in iterations - first is just list of services and their prices (as cents)
+public class Services {
+    // TODO: 2nd - change SERVICES to a List
+    public final String[] SERVICES = {"Malware Removal", "Hardware Repair",
+            "Phone Screen Replacement", "Screen Protector Install", "Data Recovery"};
+    public final long[] prices = {15000, 15000, 4000, 700, 28000};
 }
